711b127a3132b4360920b9a2772b48fbfa904270
Pradeep1422 <pradeepreddy23.a@gmail.com>
2023-06-21 13:09:34 -0400

    Initial Commit for this project

---
+++ src/Arrays.java
@@ -0,0 +1,14 @@
+public class Arrays {
+    public static void main(String[] args){
+        int[] array = new int[5];
+        array[0] = 5;
+        array[1] = 6;
+        array[2] = 7;
+        array[3] = 8;
+        array[4] = 9;
+        for(int i =0; i<array.length; i++){
+            System.out.println(array[i]);
+        }
+    }
+}
+
